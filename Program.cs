f29f328bb13702e7672b6587183f70d3caf4ffc2
Gregg Jensen <gregg@innovadev.net>
2021-01-07 10:34:49 -0700

    initial code

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace sync_geotab
+{
+    class Program
+    {
+        static Task Main(string[] args) => LoadData(args);
+
+        static string getSettingValue(string key)
+        {
+            string connStr = Properties.Settings.Default.dbConnectionString;
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            conn.Open();
+
+            string sql = "select value from settings where setting_key = 'exceptionToken'";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            string value = (string) cmd.ExecuteScalar();
+
+            return value;
+        }
+        static void setSettingsValue(string key, string value)
+        {
+            string connStr = Properties.Settings.Default.dbConnectionString;
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            conn.Open();
+
+            string sql = "update settings set value = '" + value + "' where setting_key = '" + key + "'";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+        }
+
+        static async Task LoadData(String[] args)
+        {
+            Console.WriteLine("Beginning Execution");
+
+            string database = Properties.Settings.Default.geo_database;
+            string server = Properties.Settings.Default.geo_server;
+            string user = Properties.Settings.Default.geo_user;
+            string password = Properties.Settings.Default.geo_password;
+            string path = Properties.Settings.Default.geo_export_path;
+
+            Console.WriteLine("server: " + server);
+            Console.WriteLine("database: " + database);
+            Console.WriteLine("user: " + user);
+
+            Console.WriteLine("Getting Exception Token Setting");
+            long exceptionToken = long.Parse(getSettingValue("exceptionToken"));
+            Console.WriteLine(exceptionToken);
+            Worker geotabFeed = new DatabaseWorker(user, password, database, server, null, null, null, null, exceptionToken, path);
+            Console.WriteLine("Retrieving Geotab data");
+            await geotabFeed.DoWorkAsync(false);
+
+            Console.WriteLine("Updating Exception Token");
+            setSettingsValue("exceptionToken", geotabFeed.feedParameters.LastExceptionToken.ToString());
+
+            return;
+        }
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sync_geotab
+{
+    abstract class Worker
+    {
+        readonly string path;
+        bool stop;
+        internal FeedResultData results;
+        internal FeedParameters feedParameters;
+        internal FeedProcessor feedService;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Worker"/> class.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        internal Worker(string path)
+        {
+            this.path = path;
+        }
+
+        /// <summary>
+        /// Displays the feed results.
+        /// </summary>
+        /// <param name="results">The results.</param>
+        public async Task DisplayFeedResultsAsync(FeedResultData results)
+        {
+            // Output to console
+            // new FeedToConsole(results.GpsRecords, results.StatusData, results.FaultData).Run();
+            // Optionally we can output to csv or google doc:
+            new FeedToCsv(path, results.GpsRecords, results.StatusData, results.FaultData, results.Trips, results.ExceptionEvents).Run();
+
+            // new FeedToBigquery(path).Run();
+            await Task.Delay(1000);
+        }
+
+        public async Task FeedResultToMySQL(FeedResultData results)
+        {
+            // Output to console
+            // new FeedToConsole(results.GpsRecords, results.StatusData, results.FaultData).Run();
+            // Optionally we can output to csv or google doc:
+            new FeedToMySql(results.GpsRecords, results.StatusData, results.FaultData, results.Trips, results.ExceptionEvents).Run();
+
+            // new FeedToBigquery(path).Run();
+            await Task.Delay(1000);
+        }
+
+        /// <summary>
+        /// Do the work.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        public async Task DoWorkAsync(bool continuous)
+        {
+            do
+            {
+                await WorkActionAsync();
+            }
+            while (continuous && !stop);
+        }
+
+        /// <summary>
+        /// Requests to stop.
+        /// </summary>
+        public void RequestStop()
+        {
+            stop = true;
+        }
+
+        /// <summary>
+        /// The work action.
+        /// </summary>
+        public abstract Task WorkActionAsync();
+
+    }
+}
